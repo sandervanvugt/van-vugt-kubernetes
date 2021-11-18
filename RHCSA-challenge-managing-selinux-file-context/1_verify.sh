273ef478b8459ac4008251a25460202e300396dd
sandervanvugt <mail@sandervanvugt.nl>
2021-11-18 11:38:52 +0100

    message

--- RHCSA-challenge-managing-selinux-file-context/1_verify.sh
+++ RHCSA-challenge-managing-selinux-file-context/1_verify.sh
@@ -1,2 +1,5 @@
-ps aux | grep vsftpd | grep -v grep &&\
-true
+systemctl is-active vsftpd &&\
+ls -ld /var/ftp/pub | grep 'drwxrwxrwx' &&\
+grep -i anonymous_enable=YES /etc/vsftpd/vsftpd.conf &&\
+grep -i anon_upload_enable=YES /etc/vsftpd/vsftpd.conf
+
