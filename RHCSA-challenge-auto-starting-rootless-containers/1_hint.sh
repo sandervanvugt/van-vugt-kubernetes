b6d57b6ce325e17f300eb665d443a49a8a1a7c10
sandervanvugt <mail@sandervanvugt.nl>
2021-11-20 14:24:48 +0100

    message

--- RHCSA-challenge-auto-starting-rootless-containers/1_hint.sh
+++ RHCSA-challenge-auto-starting-rootless-containers/1_hint.sh
@@ -12,9 +12,9 @@ if [[ $seconds_sofar -ge 10 &&  $seconds_sofar -lt 20 ]]; then
 fi
 
 # This hint message will appear 20+ seconds elapsed after the task began
-if [[ $seconds_sofar -ge 20 && $seconds_sofar -lt 120]]; then
-  echo "**Hint:** Have a look at the loginctl command to enable the user to auto-start systemd services without logging in." 
+if [[ $seconds_sofar -gt 20 && $seconds_sofar -lt 120]]; then
+  echo '**Hint:** Have a look at the loginctl command to enable the user to auto-start systemd services without logging in.' 
 fi
 
 if [ $seconds_sofar -ge 120 ]; then
-  echo "**Hint:** Having a hard time? No problem! Just have a look at lesson 27.4 of the [RHCSA Complete Video Course 3/ed](https://learning.oreilly.com/videos/red-hat-certified/9780135656495/9780135656495-RCSA_04_27_06/) for everything you need to know about this topic."
+  echo '**Hint:** Having a hard time? No problem! Just have a look at lesson 27.4 of the [RHCSA Complete Video Course 3/ed](https://learning.oreilly.com/videos/red-hat-certified/9780135656495/9780135656495-RCSA_04_27_06/) for everything you need to know about this topic.'
