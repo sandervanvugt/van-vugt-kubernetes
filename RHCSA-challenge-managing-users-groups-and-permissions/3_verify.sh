45991e1f6a95b289a4d242a0ad8bdc16a8cc3178
sandervanvugt <mail@sandervanvugt.nl>
2021-11-20 14:20:36 +0100

    message

--- RHCSA-challenge-managing-users-groups-and-permissions/3_verify.sh
+++ RHCSA-challenge-managing-users-groups-and-permissions/3_verify.sh
@@ -1,2 +1,2 @@
-ls -ld /data/account | grep 'drwxrws--T 2 anna account' &&\
-ls -ld /data/sales | grep 'drwxrws--T 2 linda sales'
+ls -ld /data/account | grep 'drwxrws--T. 2 anna account' &&\
+ls -ld /data/sales | grep 'drwxrws--T. 2 linda sales'
